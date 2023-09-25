420c28c64a50fd3bc80bbab873e8d4cdfc7c19c2
nawhgnues <seungdang123jp.gmail.com>
2023-09-25 22:54:02 +0900

    ~35 Building Custom Widgets

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -6,20 +6,27 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text("Hello World!"),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromRGBO(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(fontSize: 28.5, color: Colors.white),
+        ),
+      ),
+    );
+  }
 }
