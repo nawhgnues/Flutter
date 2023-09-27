c8d0b8e86ee3f1f4f2d7e5be36d8aa892e714668
nawhgnues <seungdang123jp.gmail.com>
2023-09-27 23:52:50 +0900

    ~44 Displaying Images & Using Multiple Constructor Functions

--- roll_dice_app/lib/gradient_container.dart
+++ roll_dice_app/lib/gradient_container.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice_app/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   // 위젯 트리에서 위젯이 움직일 때마다 현 상태를 보존하는 역할을 한다.
   // key를 사용하여 다른 페이지를 왔다갔다 할 수 있으며, 위젯트리에서 위치를 변경하더라도 상태정보를 유지한다.(위젯을 유니크하게 식별)
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromRGBO(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 98),
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [color1, color2], begin: startAlignment, end: endAlignment),
       ),
-      child: Center(child: StyledText()),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
+      ),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   // 위젯 트리에서 위젯이 움직일 때마다 현 상태를 보존하는 역할을 한다.
+//   // key를 사용하여 다른 페이지를 왔다갔다 할 수 있으며, 위젯트리에서 위치를 변경하더라도 상태정보를 유지한다.(위젯을 유니크하게 식별)
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//             colors: colors, begin: startAlignment, end: endAlignment),
+//       ),
+//       child: const Center(child: StyledText('Hello World!')),
+//     );
+//   }
+// }
