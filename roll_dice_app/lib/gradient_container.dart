7991f8449530e5e56d4b576dccea0d34915efa45
nawhgnues <seungdang123jp.gmail.com>
2023-09-30 23:17:49 +0900

    ~50 Module Summary - Section2 끝

--- roll_dice_app/lib/gradient_container.dart
+++ roll_dice_app/lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -15,12 +16,6 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  var activeDiceImage = 'assets/images/dice-2.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-  }
-
   @override
   Widget build(context) {
     return Container(
@@ -28,30 +23,9 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(
             colors: [color1, color2], begin: startAlignment, end: endAlignment),
       ),
-      child: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Image.asset(
-            activeDiceImage,
-            width: 200,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              // padding: const EdgeInsets.only(
-              //   top: 20,
-              // ),
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(fontSize: 28),
-            ),
-            child: const Text('Roll Dice'),
-          )
-        ],
-      )),
+      child: const Center(
+        child: DiceRoller(),
+      ),
     );
   }
 }
