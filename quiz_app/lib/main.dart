7ddc9fcf3a960e5ba73f00bf1abde37472222fa6
nawhgnues <seungdang123jp.gmail.com>
2023-10-02 00:24:32 +0900

    ~52 A Challenge For You

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -4,7 +4,39 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Text("Quiz app");
+        backgroundColor: const Color.fromARGB(255, 95, 0, 139),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/quiz-logo.png",
+                height: 300,
+              ),
+              const SizedBox(height: 100),
+              const Text(
+                "Learn Flutter the fun way!",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 95, 0, 139),
+                  side: const BorderSide(
+                    color: Colors.black,
+                    width: 0.5,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text("Start Quiz"),
+              )
+            ],
+          ),
+        ),
       ),
     ),
   );
