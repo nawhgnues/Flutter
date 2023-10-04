ceb37655bcf33798904b927a84f48f26c2818108
nawhgnues <seungdang123jp.gmail.com>
2023-10-04 22:56:19 +0900

    ~57 Repetition&Exercise: Adding a Stateful Widget

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice_app/start_screen.dart';
+import 'package:roll_dice_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 92, 16, 224),
-                Color.fromARGB(255, 197, 19, 132)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
